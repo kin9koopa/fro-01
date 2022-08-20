c5b6c6ac1d11cddb41d1f7c38fbb5d5f0149b422
Jose A. Sanchez <kin9koopa1@gmail.com>
2022-08-20 09:56:42 -0600

    added practica1

---
+++ practica1/js/index2.js
@@ -0,0 +1,8 @@
+let h2Nombre = document.getElementById('nombre-personaje');
+
+h2Nombre.textContent = h2Nombre.textContent.toUpperCase();
+
+// print the text value of the element
+console.log('Contenido: ', h2Nombre.textContent);
+console.log('Nombre de la clase: ', h2Nombre.className);
+console.log('Nombre del ID: ', h2Nombre.id);
